46efa070a8c976d540267937862b2ab17a7fac79
Aditya Sarwankar <addysarwankar@gmail.com>
2021-08-16 13:08:36 +0530

    Account & Loan MicroService added

--- account/src/main/java/com/fis/account/controller/AccountController.java
+++ account/src/main/java/com/fis/account/controller/AccountController.java
@@ -1,4 +1,4 @@
-package com.fis.account.fis.controller;
+package com.fis.account.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
